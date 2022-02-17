b67459ab813df745ff7a19e0169030676b1664c0
himelislam <himel7100@gmail.com>
2022-02-17 18:30:45 +0600

    ui update second time

--- js/script.js
+++ js/script.js
@@ -8,9 +8,16 @@ document.getElementById('calculate-button').addEventListener("click", function()
     const rentCost = getInputValue('rent-cost');
     const clothCost = getInputValue('cloth-cost');
 
+    // variables for error massage
+    const errorOne = document.getElementById('error1');
+    const errorTwo = document.getElementById('error2');
+    const errorThree = document.getElementById('error3');
+    const errorFour = document.getElementById('error4');
+
     // checking validation of input values
     if(incomeAmount>=0 && foodCost>=0 && rentCost>=0 && clothCost>=0 ){
         const totalCost = parseInt(foodCost) + parseInt(rentCost) + parseInt(clothCost);
+        errorOne.classList.add('hidden');
         if(totalCost<incomeAmount){
             const remainingAmount = incomeAmount - totalCost;
 
@@ -19,15 +26,25 @@ document.getElementById('calculate-button').addEventListener("click", function()
 
             totalExpenses.innerText = totalCost;
             balance.innerText = remainingAmount;
+
+            errorTwo.classList.add('hidden');
         }
-        // Alert massage for OverCost then Income
+        // Error massage for OverCost then Income
         else{
-            alert('Your Total Cost is More Than Your Income')
+            // Error ('Your Total Cost is More Than Your Income')
+            errorOne.classList.remove('hidden');
+            errorTwo.classList.add('hidden');
+            errorThree.classList.add('hidden');
+            errorFour.classList.add('hidden');
         } 
     }
-    // Alert massage for positive number value and number type only
+    // Error massage for positive number value and number type only
     else{
-        alert('Please Enter a Positive Number Value')
+        // Error ('Please Enter a Positive Number Value')
+        errorOne.classList.add('hidden');
+        errorTwo.classList.remove('hidden');
+        errorThree.classList.add('hidden');
+        errorFour.classList.add('hidden');
     }
 })
 
@@ -41,7 +58,7 @@ function getInputValue(inputId){
 }
 
 
-// Added event handler to save button
+// Added event handler at save button
 
 document.getElementById('save-button').addEventListener('click', function(){
     // income, balance, savings declaired variables
@@ -52,9 +69,16 @@ document.getElementById('save-button').addEventListener('click', function(){
     const saving = document.getElementById('saving-input');
     const savingRate = parseInt(saving.value);
 
+    // variables for error massage
+    const errorOne = document.getElementById('error1');
+    const errorTwo = document.getElementById('error2');
+    const errorThree = document.getElementById('error3');
+    const errorFour = document.getElementById('error4');
+
     // checking validation for Saving Input
     if(savingRate>=0){
         const savingTotal = incomeAmount *(savingRate/100);
+        errorThree.classList.add('hidden');
 
         // checking validation for sufficient amount
         if(savingTotal<=balance){
@@ -62,14 +86,24 @@ document.getElementById('save-button').addEventListener('click', function(){
             savingAmount.innerText = savingTotal;
             const remainingBalance = document.getElementById('remain-balance');
             remainingBalance.innerText = balance - savingTotal;
+
+            errorFour.classList.add('hidden');
         }
-        // Alert massage for insufficient balance
+        // Error massage for insufficient balance
         else{
-            alert('insufficient balance for saving money!!')
+            // Error('insufficient balance for saving money!!')
+            errorOne.classList.add('hidden');
+            errorTwo.classList.add('hidden');
+            errorThree.classList.remove('hidden');
+            errorFour.classList.add('hidden');
         }
     }
-    // Alert massage for invalid saving input
+    // Error massage for invalid saving input
     else{
-        alert('Enter A valid value at Saving Input!!')
+        // Error('Enter A valid value at Saving Input!!')
+        errorOne.classList.add('hidden');
+        errorOne.classList.add('hidden');
+        errorThree.classList.add('hidden');
+        errorFour.classList.remove('hidden');
     }
 })
